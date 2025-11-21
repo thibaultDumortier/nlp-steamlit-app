212edf863a2ff9b9aebcf70d71efb2c9cc3d9b82
thibaultDumortier <Thib.Dumortier@gmail.com>
2025-11-21 12:02:57 +0100

    Created branch sentence-count and sentence-count functionality

--- src/sentence_count.py
+++ src/sentence_count.py
@@ -1,2 +1,5 @@
 def count_sentences(txt:str) -> str:
-    return """"""
+    split_on_period = txt.split(". ")
+    split_on_exclam = [x.split("! ") for x in split_on_period]
+    final_split = [x.split("? ") for s in split_on_exclam for x in s]
+    return len([x for s in final_split for x in s])
